9381b5c5abadde70f8bc85af9392cd6b5a4bd266
Priyanshu000007 <ranjan.ravian@gmail.com>
2026-09-11 17:59:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -175,18 +175,14 @@ app.get("/api/personalized", async (req, res) => {
 
   } catch (error) {
 
-    console.error("Personalized weather error:", error);
+  console.error("Personalized weather error:", error);
 
-    res.status(500).json({
+  res.status(500).json({
+    error: "Unable to generate personalized weather information",
+    details: error.message
+  });
 
-      error: "Unable to generate personalized weather information"
-
-    });
-
-  }
-
-});
-
+}
 
 // ------------------------------------
 // START SERVER
